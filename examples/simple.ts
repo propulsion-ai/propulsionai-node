758e2f17be03815120b061c914b02a0dce7c4b75
Rohit Hazra <rohithzr@live.com>
2024-06-06 21:00:32 -0700

    add chat with auto function calling

--- examples/simple.ts
+++ examples/simple.ts
@@ -7,11 +7,17 @@ const propulsionai = new Propulsionai({
 });
 
 async function mongo_query(parameters: any){
-
+  // console.log(parameters);
+  return {
+    "_id": "",
+    "result": {
+      "amount": "4345300"
+    }
+  }
 }
 
 async function main() {
-  const modelRunResponse = await propulsionai.models.chat('khzhgybzctm6e8m', {
+  const modelRunResponse = await propulsionai.models.chatAuto('khzhgybzctm6e8m', {
     tools: [{
       type: 'function',
       function: {
@@ -60,12 +66,6 @@ async function main() {
   console.log(modelRunResponse.id);
   console.log(modelRunResponse.choices);
   console.log(modelRunResponse.toolCalls);
-  
-  if(modelRunResponse.toolCalls){
-    modelRunResponse.toolCalls.forEach((toolCall: any) => {
-      console.log(JSON.stringify(toolCall));
-    });
-  }
 }
 
 main();
