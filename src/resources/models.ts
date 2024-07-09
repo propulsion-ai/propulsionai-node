a230b4abf18fa58ef99ce5589e90dad1433eefc3
Rohit Hazra <rohithzr@live.com>
2024-07-08 17:54:24 -0700

    fix types

--- src/resources/models.ts
+++ src/resources/models.ts
@@ -82,8 +82,7 @@ export class Models extends APIResource {
       if (inital_response.toolCalls) {
         for (let i in inital_response.toolCalls) {
           const toolCall: any = inital_response.toolCalls[i];
-          const uf: any = body.tools?.find((t) => t.function.name === toolCall.function.name)?.function
-            .function;
+          const uf: any = body.tools?.find((t) => t.function.name === toolCall.function.name)?.function;
           if (!uf) {
             return inital_response;
           }
@@ -401,13 +400,40 @@ export namespace ModelEpParams {
        * The parameters the functions accepts, described as a JSON Schema object.
        */
       parameters?: Record<string, unknown>;
-    }
-  }
 
       /**
        * The function to be called. Must be a valid JavaScript function.
        */
       function?: (parameters: any) => any;
+    }
+  }
+  export interface Tool {
+    function: Tool.Function;
+
+    /**
+     * The type of the tool. Currently, only `function` is supported.
+     */
+    type: 'function';
+  }
+
+  export namespace Tool {
+    export interface Function {
+      /**
+       * The name of the function to be called. Must be a-z, A-Z, 0-9, or contain
+       * underscores and dashes, with a maximum length of 64.
+       */
+      name: string;
+
+      /**
+       * A description of what the function does, used by the model to choose when and
+       * how to call the function.
+       */
+      description?: string;
+
+      /**
+       * The parameters the functions accepts, described as a JSON Schema object.
+       */
+      parameters?: Record<string, unknown>;
     }
   }
 }
